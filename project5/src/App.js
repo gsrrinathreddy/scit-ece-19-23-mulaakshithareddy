d9a716b5d9a1f44a47cfa4f6666b514f4724292c
Mulaakshitha <mulaakshithareddy@gmail.com>
2023-03-29 11:39:58 -0700

    project5 added

--- project5/src/App.js
+++ project5/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Components/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Aboutme from './Pages/Aboutme';
+import Hobbies from './Pages/Hobbies';
+import Qualifications from './Pages/Qualifications';
+import Skills from './Pages/Skills';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     <Navbar/>
+     <Routes>
+     <Route path="Aboutme" element={<Aboutme/>}/>
+     <Route path="Hobbies" element={<Hobbies/>}/>
+     <Route path="Qualifications" element={<Qualifications/>}/>
+     <Route path="Skills" element={<Skills/>}/>
+
+      </Routes>
+      </BrowserRouter>
+      
+     </div>
   );
 }
 
