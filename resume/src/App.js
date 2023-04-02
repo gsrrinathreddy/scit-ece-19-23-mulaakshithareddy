1b61745e3b060476aa5ad5e307fa4699ebd4f8df
Mulaakshitha <mulaakshithareddy@gmail.com>
2023-04-01 21:11:27 -0700

    resume added

---
+++ resume/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Aboutme from './Pages/Aboutme';
+import Experience from './Pages/Experience';
+import Hobbies from './Pages/Hobbies';
+import Qualifications from './Pages/Qualifications';
+import Skills from './Pages/Skills';
+function App() {
+  return (
+    <div className="App">
+    <BrowserRouter>
+     <Navbar/>
+     <Routes>
+     <Route path="Aboutme" element={<Aboutme/>}/>
+     <Route path="Experience" element={<Experience/>}/>
+     <Route path="Hobbies" element={<Hobbies/>}/>
+     <Route path="Qualifications" element={<Qualifications/>}/>
+     <Route path="Skills" element={<Skills/>}/>
+      
+      </Routes>
+      </BrowserRouter>
+      </div>
+  );
+}
+
+export default App;
